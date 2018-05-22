aa211f04a5590e562af3e56fac827c7d9f0ea481
Evgeny <instane@ya.ru>
2018-05-22 12:36:23 +0300

    +ru comments, structure cleanup, arg parser

--- pcs.py
+++ pcs.py
@@ -1,89 +1,88 @@
-import cv2, sys, pytesseract
-import numpy as np
-
-def _exit():
-    k = cv2.waitKey(0) & 0xFF
-    if k == 27:         # wait for ESC key to exit
-        cv2.destroyAllWindows()
-        sys.exit(0)
-
-
-def putContNum(cont_num, x, y):
-    font                   = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (x+1,y+8)
-    fontScale              = 0.3
-    fontColor              = (0,0,0)
-    lineType               = 2
-      
-    cv2.putText(imgcolor,
-            "{}".format(cont_num),
-            bottomLeftCornerOfText,
-            font,
-            fontScale,
-            fontColor,
-            lineType)
-
-
-img = cv2.imread(sys.argv[1],0)
-
-den = cv2.fastNlMeansDenoising(img, None, 21, 5, 21)
-img = den
-thresh = cv2.threshold(img,127,255,0)[1]
-thfake = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)[1]
-print('thresh', thresh)
-print('thfake', thfake)
-print(cv2.THRESH_BINARY_INV)
-cv2.imshow('opencv-result', den)
-cv2.imshow('opencv-resultor', img)
-cv2.imshow('opencv-resulth', thresh)
-#_exit()
-
-imgcolor = cv2.imread(sys.argv[1])
-ret,thresh = cv2.threshold(img,127,255,0)
-imcont,contours,hierarchy = cv2.findContours(thresh, 1, 2)
-cv2.imwrite('imcontsav.png',imcont)
-imcontsavcolor = cv2.imread('imcontsav.png')
-
-print("contours len", len(contours))
-i = 0
-areas = {}
-for cont in contours:
-    i+=1
-    areas[i] = cv2.contourArea(cont)
-    print("cont {} area = ".format(i), cv2.contourArea(cont))
-    print(cont)
-
-print('areas', areas)
-areas_sorted = sorted(areas.items(), key=lambda x: x[1], reverse=True)
-print('areas_sorted', areas_sorted)
-
-for i in range(0,len(areas)):
-    cont_num = areas_sorted[i][0] - 1
-    print(i, areas_sorted[i], areas_sorted[i][0])
-    print("cont {} area = ".format(cont_num), cv2.contourArea(contours[cont_num]))
-    if cont_num != 999:
-        x,y,w,h = cv2.boundingRect(contours[cont_num])
-#        putContNum(cont_num,x,y)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-        cv2.rectangle(imgcolor,(x,y),(x+w,y+h),(0,255,0),1)
-        cv2.rectangle(imcontsavcolor,(x,y),(x+w,y+h),(0,255,0),1)
-
-def to_rgb(img):
-    return cv2.merge([img] * 3)
 '''
-res = np.concatenate((
-    to_rgb(img),
-    to_rgb(imcont)))
-
-cv2.imshow('opencv-result', res)
+argparse - Обработка входных параметров
+cv2 - Обертка OpenCV для Python
+pytesseract - Обертка для Tesseract(OCR)
+pyplot из matplotlib - Отрисовки графики
+floor и ceil из math - Округление расчётов
 '''
-cv2.imshow("img", img)
-cv2.imshow("imgcolor", imgcolor)
-cv2.imshow("imcont", imcont)
-cv2.imshow("imcontsavcolor", imcontsavcolor)
+import argparse, cv2, pytesseract 
+from matplotlib import pyplot as plt
+from math import floor, ceil
 
-print("Possible solustion is ", pytesseract.image_to_string(imcont, config='nobatch digits'))
+# инициализация парсера аргументов, задание описания программы
+parser = argparse.ArgumentParser(description='Captcha symbol recognition.')
+# добавление аргумента изображения
+parser.add_argument('image', metavar='I', type=str, help='an image for the recognition')
+# добавление аргумента метода распознавания изображения
+parser.add_argument('--method', '-m', dest='method', default='ocr',
+                   help='recognition method (default: ocr)')
 
-k = cv2.waitKey(0) & 0xFF
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
+# считывание входных параметров
+args = parser.parse_args()
+
+# Функция для представления данных при помощи pyplot
+def present_data(images_dict):
+    x, y = ceil((len(images_dict)) / 2), floor((len(images_dict)) / 2) # определяем размерность сетки
+    num = 0 # номер изображения
+    for key, value in images_dict.items(): # цикл для прогона по всем изображением
+        plt.subplot(x, y, num+1) # обозначаем место для изображения
+        plt.imshow(value, 'gray') # показываем изображение
+        plt.title(key) # пишем название
+        num += 1 # увеличиваем номер изображения
+    plt.show() # показываем созданное окно
+
+# Функция для определения объектов на изображение(найденные помещаем в прямоугольник)
+def draw_rectangle(image, contours):
+    print("Contours length is", len(contours)) # вывод количества найденных контуров
+    i = 0 # переменная счетчик
+    areas = {} # словарь для координат прямоугольника
+    for cont in contours: # цикл для каждой точки найденных контуров
+        i += 1 # инкрементируем счетчик
+        areas[i] = cv2.contourArea(cont) # вычисляет область контура
+    areas_sorted = sorted(areas.items(), key=lambda x: x[1], reverse=True) # сортируем координаты
+    for i in range(0,len(areas)): # цикл для всех найденых координат
+        cont_num = areas_sorted[i][0] - 1 # номер контура
+        if cont_num != 999: # если контур не равен 999
+            x,y,w,h = cv2.boundingRect(contours[cont_num]) # вычисляет вершины прямоугольника
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1) # рисуем прямоугольник на изображении
+    return image
+
+# Функция для определения контуров
+def find_contours(image):
+    imcont, contours, hierarchy = cv2.findContours(image, 1, 2) # находим контуры
+    return contours # возвращаем контуры
+
+# Функция для загрузки изображения
+def load_image(image_path):
+    return cv2.imread(image_path) # загружаем и возвращаем изображение
+
+# Функция для предварительной обработки
+def preprocess(image):
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # обесцвечиваем
+    den = cv2.fastNlMeansDenoising(img_gray, None, 21, 5, 21) # фильтр для удаления шума
+    thresh = cv2.threshold(den,127,255,0)[1] # бинарное изображение
+    return den, thresh
+
+# Функция для распознавания символов
+def recognize(image, method):
+    if method == 'ocr': # если метов ocr
+        # то пытаемся определить тесерактом
+        return pytesseract.image_to_string(image, config='nobatch digits').replace(' ', '')
+    else: # иначе
+        return 'NOT IMPLEMENTED YET' # не реализовано
+
+# основная часть
+img = load_image(args.image) # загружаем изображение
+den, thresh = preprocess(img) # проводим предварительную обработку
+contours = find_contours(thresh) # находим контуры
+
+cv2.imwrite('temp.png',thresh) # сохраняем промежуточное изображение
+imcont = cv2.imread('temp.png') # считываем для дальнейшей обработки
+
+imcont = draw_rectangle(imcont, contours); # отрисовываем найденные контуры
+
+# выводим результата обработки
+print("Possible solution with {} method is {}".format(args.method, recognize(thresh, args.method)))
+
+# показываем полученный результат
+present_data({'orig': img, 'fastNlMeansDenoising': den, 'thresh': thresh, 'rectangle': imcont})
